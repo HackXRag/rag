5bb02d88616819f5043e8d481ca363399858c512
Thomas Brettin <brettin@anl.gov>
2024-08-07 17:35:47 -0500

    turned it into a function

--- embed.py
+++ embed.py
@@ -1,27 +1,36 @@
 from openai import OpenAI
 
 # Modify OpenAI's API key and API base to use vLLM's API server.
-openai_api_key = "EMPTY"
-openai_api_base = "http://localhost:8000/v1"
 
-client = OpenAI(
+def embed(texts):
+    
+    openai_api_key = "EMPTY"
+    openai_api_base = "http://rbdgx1.cels.anl.gov:8000/v1"
+
+    client = OpenAI(
     # defaults to os.environ.get("OPENAI_API_KEY")
-    api_key=openai_api_key,
-    base_url=openai_api_base,
-)
+        api_key=openai_api_key,
+        base_url=openai_api_base,
+    )
 
-models = client.models.list()
-model = models.data[0].id
-print(f'Using model: {model}')
+    models = client.models.list()
+    model = models.data[0].id
+    print(f'Using model: {model}')
 
-responses = client.embeddings.create(
-    input=[
+    responses = client.embeddings.create(
+        input=texts,
+        model=model,
+        encoding_format="float",
+    )
+    return responses
+
+    
+if __name__ == "__main__":
+    texts = [
         "Hello my name is",
         "The best thing about vLLM is that it supports many different models"
-    ],
-    model=model,
-    encoding_format="float",
-)
-
-for data in responses.data:
-    print(data.embedding)  # list of float of len 4096
+    ]
+    
+    responses = embed(texts)
+    for data in responses.data:
+        print(data.embedding)  # list of float of len 4096
