0fc5749ca4d6e035c0fb1b4348f8736cd3f4e662
Thomas Brettin <brettin@anl.gov>
2024-08-22 10:20:40 -0500

    added average

--- parse_vllm_log.pl
+++ parse_vllm_log.pl
@@ -1,5 +1,11 @@
 #!/usr/bin/perl
 
 while(<>){
-  print "$1\n" if /(Avg prompt throughput: \d+\.*\d* tokens\/s, Avg generation throughput: \d+\.*\d* tokens\/s, Running: \d+ reqs)/;
+  print "$1" if /(Avg prompt throughput: \d+\.*\d* tokens\/s, Avg generation throughput: \d+\.*\d* tokens\/s, Running: \d+ reqs)/;
+  if (/Avg prompt throughput: (\d+\.*\d*) tokens\/s, Avg generation throughput: \d+\.*\d* tokens\/s, Running: \d+ reqs/)
+  {
+	  $sum = $sum + $1;
+	  $count = $count + 1;
+	  print ("\t", $sum/$count, "\n");
+  }
 }
